18456441719a046df37f829e6af9e41058921ed4
hmini <15357082399@163.com>
2019-06-21 13:29:13 +0800

    e

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 import router from './router';
 import ElementUi from 'element-ui';
 import Vuex from 'vuex';
 import 'element-ui/lib/theme-chalk/index.css';
+import App from './App';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import echarts from 'echarts';
